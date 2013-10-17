c9759aeb65c5ac42091f8ebc3666465bbc2ca6a0
Pat Scott <patscott@physics.mcgill.ca>
2013-10-16 21:30:02 -0400

    Tweaks to C/C++ wrapper.  Now compiles, still not tested though.
    	modified:   source/cwrapper.f90

--- Fortran/source/cwrapper.f90
+++ Fortran/source/cwrapper.f90
@@ -4,10 +4,11 @@
 !
 ! Allows one to call DEvoPack using the C prototypes
 !
-! void runDE(double (*minusloglike)(double*, int, bool, bool), double(*prior)(double*), double lowerbounds[], double upperbounds[], 
-!            char path[], int nDerived, int discrete[], bool partitionDiscrete, int maxciv, int maxgen, int NP, double F[],
-!            double Cr, double lambda, bool current, bool expon, int bndry, bool jDE, bool lambdajDE,
-!            bool removeDuplicates, bool doBayesian, double maxNodePop, double Ztolerance, int savecount, bool resume)
+! void runDE(double (*minusloglike)(double*, int, bool, bool), double(*prior)(double*), int nPar, double lowerbounds[], 
+!            double upperbounds[], char path[], int nDerived, int nDiscrete, int discrete[], bool partitionDiscrete, 
+!            int maxciv, int maxgen, int NP, int nF, double F[], double Cr, double lambda, bool current, bool expon, 
+!            int bndry, bool jDE, bool lambdajDE, bool removeDuplicates, bool doBayesian, double maxNodePop, 
+!            double Ztolerance, int savecount, bool resume)
 !
 ! double minusloglike(double* params, int fcall, bool quit, bool validvector)
 !
@@ -19,36 +20,38 @@ module de_c_interface
 
 contains
 	
-    subroutine runDE(minusloglike, prior, lowerbounds, upperbounds, path, nDerived, discrete, partitionDiscrete, &
-                    maxciv, maxgen, NP, F, Cr, lambda, current, expon, bndry, jDE, lambdajDE,                    &
-                    removeDuplicates, doBayesian, maxNodePop, Ztolerance, savecount, resume) bind(c)
+    subroutine runDE(minusloglike, prior, nPar, lowerbounds, upperbounds, path, nDerived, nDiscrete, &
+                     discrete, partitionDiscrete, maxciv, maxgen, NP, nF, F, Cr, lambda, current, expon, &
+                     bndry, jDE, lambdajDE, removeDuplicates, doBayesian, maxNodePop, Ztolerance, savecount, &
+                     resume) bind(c)
 
     use iso_c_binding, only: c_int, c_bool, c_double, c_char, c_funptr, C_NULL_CHAR
     use de, only: run_de
+    use detypes, only: dp
     implicit none
 
     type(c_funptr),  intent(in), value :: minusloglike, prior
-    integer(c_int),  intent(in), value :: nDerived, maxciv, maxgen, NP, bndry, savecount
-    integer(c_int),  intent(in)        :: discrete(:)
+    integer(c_int),  intent(in), value :: nPar, nDerived, nDiscrete, maxciv, maxgen, NP, nF, bndry, savecount
+    integer(c_int),  intent(in)        :: discrete(nDiscrete)
     logical(c_bool), intent(in), value :: partitionDiscrete, current, expon, jDE, lambdajDE, removeDuplicates, doBayesian, resume
     real(c_double),  intent(in), value :: Cr, lambda, maxNodePop, Ztolerance
-    real(c_double),  intent(in)        :: lowerbounds(:), upperbounds(:), F(:)
+    real(c_double),  intent(in)        :: lowerbounds(nPar), upperbounds(nPar), F(nF)
     character(kind=c_char,len=1), dimension(1), intent(in) :: path
 
     integer :: i
     integer, parameter :: maxpathlen = 300
-    character(len=maxpathlen) :: fortran_path
+    character(len=maxpathlen) :: path_f
 
-    fortran_path = ' '
+    path_f = ' '
     do i = 1, maxpathlen
        if (path(i) == C_NULL_CHAR) then
           exit
        else
-	  fortran_path(i) = path(i)
+	  path_f(i:i) = path(i)
        end if
     end do
 
-    call run_de(minusloglike_f, prior_f, lowerbounds, upperbounds, path, nDerived=nDerived, discrete=discrete,    &
+    call run_de(minusloglike_f, prior_f, lowerbounds, upperbounds, path_f, nDerived=nDerived, discrete=discrete,  &
                 partitionDiscrete=logical(partitionDiscrete), maxciv=maxciv, maxgen=maxgen, NP=NP, F=F, Cr=Cr,    &
                 lambda=lambda, current=logical(current), expon=logical(expon), bndry=bndry, jDE=logical(jDE),     &
                 lambdajDE=logical(lambdajDE), removeDuplicates=logical(removeDuplicates),                         &
@@ -62,18 +65,18 @@ contains
 	  
        implicit none
 
-       integer          :: fcall
-       double precision :: params(:)
-       logical          :: quit, validvector
+       integer(c_int)  :: fcall
+       real(c_double)  :: params(:)
+       logical(c_bool) :: quit, validvector
 
        interface
           real(c_double) function minusloglike_proto(params, fcall, quit, validvector)
              use iso_c_binding, only: c_int, c_double, c_bool
              implicit none
-             real(c_double),  intent(inout) :: params(:)
-             integer(c_int),  intent(inout) :: fcall 
-             logical(c_bool), intent(out)   :: quit
-             logical(c_bool), intent(in)    :: validvector
+             real(c_double),  intent(inout)     :: params(:)
+             integer(c_int),  intent(inout)     :: fcall 
+             logical(c_bool), intent(out)       :: quit
+             logical(c_bool), intent(in), value :: validvector
           end function minusloglike_proto
        end interface
 
