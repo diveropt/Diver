4f69c1ff6a49f38a0812bc1bb22c889ae8b7d719
Pat Scott <patscott@physics.mcgill.ca>
2013-11-23 00:16:53 -0500

    A few tweaks to the cwrapper to enhance backwards compatibility with older versions of gfortran (specifically v4.4.5 and others that do not allow internal procedures as arguments).
    	modified:   source/cwrapper.f90

--- Fortran/source/cwrapper.f90
+++ Fortran/source/cwrapper.f90
@@ -18,19 +18,26 @@
 
 module de_c_interface
 
+use iso_c_binding, only: c_funptr
+
 implicit none
+
+private
+public cdiver
+
+type(c_funptr) :: minusloglike, prior
 
 contains
 	
-    subroutine cdiver(minusloglike, nPar, lowerbounds, upperbounds, path, nDerived, nDiscrete, discrete,  &
-                      partitionDiscrete, maxciv, maxgen, NP, nF, F, Cr, lambda, current, expon,           &
-                      bndry, jDE, lambdajDE,  convthresh, convsteps, removeDuplicates, doBayesian, prior, &
+    subroutine cdiver(minusloglike_in, nPar, lowerbounds, upperbounds, path, nDerived, nDiscrete, discrete,  &
+                      partitionDiscrete, maxciv, maxgen, NP, nF, F, Cr, lambda, current, expon,              &
+                      bndry, jDE, lambdajDE,  convthresh, convsteps, removeDuplicates, doBayesian, prior_in, &
                       maxNodePop, Ztolerance, savecount, resume, context, verbose, skip_MPI_init) bind(c)
 
     use iso_c_binding, only: c_int, c_bool, c_double, c_char, c_funptr, c_ptr, C_NULL_CHAR
     use de, only: diver
 
-    type(c_funptr),  intent(in), value :: minusloglike, prior
+    type(c_funptr),  intent(in), value :: minusloglike_in, prior_in
     type(c_ptr),     intent(inout)     :: context
     integer(c_int),  intent(in), value :: nPar, nDerived, nDiscrete, maxciv, maxgen, NP, nF, bndry, convsteps, savecount, verbose
     integer(c_int),  intent(in), target:: discrete(nDiscrete)
@@ -54,6 +61,10 @@ contains
       end function prior_f_proto
     end interface
     procedure(prior_f_proto), pointer :: priorPtr
+
+    ! Set the pointers to the likelihood and prior functions
+    minusloglike = minusloglike_in
+    prior = prior_in
 
     ! Fix up the string, which is null-terminated in C
     path_f = ' '
@@ -88,74 +99,73 @@ contains
                maxNodePop=maxNodePop, Ztolerance=Ztolerance, savecount=savecount, resume=logical(resume),      &
                context=context, verbose=verbose, skip_MPI_init=logical(skip_MPI_init))
     
-    contains
+    end subroutine
 
-       ! Wrapper for the likelihood function
-       real(dp) function minusloglike_f(params, fcall, quit, validvector, context)
-       use iso_c_binding, only: c_f_procpointer, c_ptr
-       use detypes, only: dp
+
+    ! Wrapper for the likelihood function
+    real(dp) function minusloglike_f(params, fcall, quit, validvector, context)
+    use iso_c_binding, only: c_f_procpointer, c_ptr, c_bool
+    use detypes, only: dp
   
-       real(dp),    intent(inout) :: params(:)
-       integer,     intent(inout) :: fcall
-       logical,     intent(out)   :: quit
-       logical,     intent(in)    :: validvector
-       type(c_ptr), intent(inout) :: context
-       logical(c_bool)            :: quit_c, validvector_c
+    real(dp),    intent(inout) :: params(:)
+    integer,     intent(inout) :: fcall
+    logical,     intent(out)   :: quit
+    logical,     intent(in)    :: validvector
+    type(c_ptr), intent(inout) :: context
+    logical(c_bool)            :: quit_c, validvector_c
 
-       interface
-          real(c_double) function minusloglike_proto(params, param_dim, fcall, quit, validvector, context) bind(c)
-             use iso_c_binding, only: c_int, c_double, c_bool, c_ptr
-             implicit none
-             real(c_double),  intent(inout)        :: params(*)
-             integer(c_int),  intent(in), value    :: param_dim
-             integer(c_int),  intent(inout)        :: fcall
-             logical(c_bool), intent(out)          :: quit
-             logical(c_bool), intent(in), value    :: validvector
+    interface
+       real(c_double) function minusloglike_proto(params, param_dim, fcall, quit, validvector, context) bind(c)
+          use iso_c_binding, only: c_int, c_double, c_bool, c_ptr
+          implicit none
+          real(c_double),  intent(inout)        :: params(*)
+          integer(c_int),  intent(in), value    :: param_dim
+          integer(c_int),  intent(inout)        :: fcall
+          logical(c_bool), intent(out)          :: quit
+          logical(c_bool), intent(in), value    :: validvector
+          type(c_ptr),     intent(inout)     :: context
+       end function minusloglike_proto
+    end interface
+
+    procedure(minusloglike_proto), pointer, bind(c) :: minusloglike_c
+
+       ! Cast c_funptr minusloglike to a pointer with signature minusloglike_proto, and assign the result to minusloglike_c
+       call c_f_procpointer(minusloglike,minusloglike_c)
+
+       validvector_c = validvector  ! Do boolen type conversion default logical --> c_bool 
+       minusloglike_f = minusloglike_c(params, size(params), fcall, quit_c, validvector_c, context)
+       quit = quit_c                ! Do boolen type conversion c_bool --> default logical
+
+    end function minusloglike_f
+
+
+    ! Wrapper for the prior function
+    real(dp) function prior_f(true_params, context)
+    use iso_c_binding, only: c_f_procpointer, c_ptr
+    use detypes, only: dp
+
+    real(dp),    intent(in)    :: true_params(:)
+    type(c_ptr), intent(inout) :: context
+    
+    interface
+       real(c_double) function prior_proto(true_params, true_param_dim, context) bind(c)
+          use iso_c_binding, only: c_double, c_int, c_ptr
+          implicit none
+          real(c_double),  intent(in)           :: true_params(*)
+          integer(c_int),  intent(in), value    :: true_param_dim
              type(c_ptr),     intent(inout)        :: context
-          end function minusloglike_proto
-       end interface
+       end function prior_proto
+    end interface
 
-       procedure(minusloglike_proto), pointer, bind(c) :: minusloglike_c
+    procedure(prior_proto), pointer, bind(c) :: prior_c
 
-          ! Cast c_funptr minusloglike to a pointer with signature minusloglike_proto, and assign the result to minusloglike_c
-          call c_f_procpointer(minusloglike,minusloglike_c)
+       ! Cast c_funptr prior to a pointer with signature prior_proto, and assign the result to prior_c
+       call c_f_procpointer(prior,prior_c)
 
-          validvector_c = validvector  ! Do boolen type conversion default logical --> c_bool 
-          minusloglike_f = minusloglike_c(params, size(params), fcall, quit_c, validvector_c, context)
-          quit = quit_c                ! Do boolen type conversion c_bool --> default logical
+       prior_f = prior_c(true_params, size(true_params), context)
 
-       end function minusloglike_f
+    end function prior_f
 
-
-       ! Wrapper for the prior function
-       real(dp) function prior_f(true_params, context)
-       use iso_c_binding, only: c_f_procpointer
-       use detypes, only: dp
-
-       real(dp),    intent(in)    :: true_params(:)
-       type(c_ptr), intent(inout) :: context
-       
-       interface
-          real(c_double) function prior_proto(true_params, true_param_dim, context) bind(c)
-             use iso_c_binding, only: c_double, c_int, c_ptr
-             implicit none
-             real(c_double),  intent(in)           :: true_params(*)
-             integer(c_int),  intent(in), value    :: true_param_dim
-             type(c_ptr),     intent(inout)        :: context
-          end function prior_proto
-       end interface
-
-       procedure(prior_proto), pointer, bind(c) :: prior_c
-
-          ! Cast c_funptr prior to a pointer with signature prior_proto, and assign the result to prior_c
-          call c_f_procpointer(prior,prior_c)
-
-          prior_f = prior_c(true_params, size(true_params), context)
-
-       end function prior_f
-
-
-   end subroutine
 
 
 end module
