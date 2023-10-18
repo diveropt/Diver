5eb25fa864b85f5ffe3b198309033d166efeb9f8
Pat Scott <patrickcolinscott@gmail.com>
2023-10-18 12:29:30 +1000

    Put include guards on headers, remove define none, bump up version 	modified:   HISTORY 	modified:   example_cpp/example_cpp.cpp 	modified:   include/diver.h 	modified:   include/diver.hpp 	modified:   src/init.f90

--- src/init.f90
+++ src/init.f90
@@ -15,7 +15,7 @@ implicit none
 private
 public param_assign, initialize, init_all_random_seeds
 
-character (len=*), parameter :: version_number = "1.0.6"
+character (len=*), parameter :: version_number = "1.0.7"
 
 contains
 
